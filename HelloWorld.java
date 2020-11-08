28875d3c494b9fd75897c8711bbccece103f4a73
jwiertel <73883378+jwiertel@users.noreply.github.com>
2020-11-08 22:54:09 +0100

    #change: Aufgabe 4 Julian Wiertelorz

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,9 @@
 package test;
 
+
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Studiendauer: 6 Semester");
+		//aufgabe 4 Aenderung von Julian Wiertelorz
 	}
 }
